58a0cfe425da37ed27ccd17c6d55cdfb41b8fa66
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-02-28 12:16:38 -0300

    first commit

---
+++ lib/elixir_jobs/application.ex
@@ -0,0 +1,34 @@
+defmodule ElixirJobs.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      ElixirJobs.Repo,
+      # Start the Telemetry supervisor
+      ElixirJobsWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: ElixirJobs.PubSub},
+      # Start the Endpoint (http/https)
+      ElixirJobsWeb.Endpoint
+      # Start a worker by calling: ElixirJobs.Worker.start_link(arg)
+      # {ElixirJobs.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ElixirJobs.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ElixirJobsWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
